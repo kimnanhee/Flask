3a37ce5acc375f4fa243033bb36e2e7f51bf01c3
kimnanhee <kimnanhee0225@gmail.com>
2020-08-10 22:19:08 +0900

    [Modified]

--- POST_login_input/server.py
+++ POST_login_input/server.py
@@ -3,7 +3,7 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/login", methods = ['GET','POST'])
+@app.route("/login", methods = ['POST'])
 def hello_first():
 	if request.method == 'POST':
 		input_id = request.json['id'] # id에 해당하는 값 받아오기
@@ -12,9 +12,9 @@ def hello_first():
 		print(input_id, input_password)
 
 		if (input_id=='nanhee' and input_password=='nanhee0225!'): # 받은 id, password가 일치하면 'success'출력
-			return jsonify('{ success }') # result 값 반환하기
+			return jsonify('{ state : success }') # result 값 반환하기
 		else:
-			return jsonify('{ fail to login }') # 틀리면 'fail'출력 
+			return jsonify('{ state : fail to login }') # 틀리면 'fail'출력 
 	return 'failed'
 
 if __name__ == "__main__":
