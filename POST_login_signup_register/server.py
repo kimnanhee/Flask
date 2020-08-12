2c743ad6826210b5586a64139159a3e0e2546709
kimnanhee <kimnanhee0225@gmail.com>
2020-08-12 09:26:18 +0900

    [Modified] post_login_signup_register

--- POST_login_signup_register/server.py
+++ POST_login_signup_register/server.py
@@ -12,6 +12,8 @@ def nan():
 		set_id = request.json['id'] # id 설정하기
 		set_password = request.json['password'] # password 설정하기
 
+		if set_id in set_information: # 딕셔너리에 id가 존재할 때
+			return jsonify('{ state : defined_id }')
 		set_information[set_id]=set_password # id, password 딕셔너리에 저장
 	return jsonify('{ state : success }')
 	
@@ -22,10 +24,13 @@ def hee():
 		input_id = request.json['id'] # id에 해당하는 값 받아오기
 		input_password = request.json['password'] # password에 해당하는 값 받아오기
 
-		if(set_information[input_id]==input_password): # id 키 값에 맞는 password가 있으면
-			return jsonify('{ state : success }') # success 반환
-		else:
-			return jsonify('{ state : fail_to_login }')
+		if input_id in set_information: # 딕셔너리에 id가 존재할 때
+			if(set_information[input_id]==input_password): # id 키 값에 맞는 password가 있으면
+				return jsonify('{ state : success }') # success 반환
+			else:
+				return jsonify('{ state : fail_to_login }')
+		else: # 딕셔너리에 id가 존재하지 않을 때
+			return jsonify('{ state : undefined_id }')
 	return 'failed'
 
 if __name__ == "__main__":
